8fddfa8db20854f91ba5447ef7e60e284d257d87
michealodwyer26 <michealodwyer26@gmail.com>
2019-06-13 19:11:07 +0100

    Basic window and event loop set up.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+use std::thread::sleep;
+
+pub fn main() {
+    let sdl_context = sdl2::init().expect("SDL initialization failed");
+    let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsystem");
+
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .expect("Failed to create window");
+
+    let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().expect("Failed to
+    get SDL event pump");
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => { break 'running },
+                _ => {}
+            }
+        }
+        sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
