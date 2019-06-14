bad23b5aa63d0cebf9dc3fbbb9ffd828f443e599
michealodwyer26 <michealodwyer26@gmail.com>
2019-06-14 13:52:34 +0100

    Implemented the Tetris struct for overall game functionality (level, current piece, etc).

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,94 @@ enum TextureColour {
     Blue
 }
 
+struct Tetris {
+    game_map: Vec<Vec<u8>>,
+    current_level: u32,
+    score: u32,
+    num_lines: u32,
+    current_piece: Option<Tetrimino>
+}
+
+impl Tetris {
+    fn new() -> Tetris {
+        let mut game_map = Vec::new();
+        for _ in 0..16 {
+            game_map.push(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+        Tetris {
+            game_map: game_map,
+            current_level: 1,
+            score: 0,
+            num_lines: 0,
+            current_piece: None
+        }
+    }
+
+    fn create_new_tetrimino(&self) -> Tetrimino {
+        static mut PREV: u8 = 7;
+        let mut rand_num = rand::random::<u8>() % 7;
+        if unsafe {PREV} == rand_num {
+            rand_num = rand::random::<u8>() % 7;
+        }
+        unsafe { PREV = rand_num; }
+        match rand_num {
+            0 => TetriminoI::new(),
+            1 => TetriminoJ::new(),
+            2 => TetriminoL::new(),
+            3 => TetriminoO::new(),
+            4 => TetriminoS::new(),
+            5 => TetriminoZ::new(),
+            6 => TetriminoT::new(),
+            _ => unreachable!()
+        }
+    }
+
+    fn check_lines(&mut self) {
+        let mut y = 0;
+
+        while y < self.game_map.len() {
+            let mut complete = true;
+
+            for x in &self.game_map[y] {
+                if *x == 0 {
+                    complete = false;
+                    break
+                }
+            }
+            if complete {
+                self.game_map.remove(y);
+                y -= 1;
+            }
+            y += 1;
+        }
+        while self.game_map.len() < 16 {
+            self.game_map.insert(0, vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+    }
+
+    fn make_permanent(&mut self) {
+        if let Some(ref mut piece) = self.current_piece {
+            let mut shift_y = 0;
+
+            while shift_y < piece.states[piece.current_state as usize].len() && piece.y + shift_y < self.game_map.len() {
+                let mut shift_x = 0;
+
+                while shift_x < piece.states[piece.current_state as usize][shift_y].len() && (piece.x + shift_x as isize) < 
+                    self.game_map[piece.y + shift_y].len() as isize {
+                    if piece.states[piece.current_state as usize][shift_y][shift_x] != 0 {
+                        let x = piece.x + shift_x as isize;
+                        self.game_map[piece.y + shift_y][x as usize] = piece.states[piece.current_state as usize][shift_y][shift_x];
+                    }
+                    shift_x += 1;
+                }
+                shift_y += 1;
+            }
+        }
+        self.check_lines();
+        self.current_piece = None;
+    }
+}
+
 struct Tetrimino {
     states: States,
     x: isize,
@@ -245,24 +333,24 @@ impl TetriminoGenerator for TetriminoT {
     }
 }
 
-fn create_new_tetrimino() -> Tetrimino {
-    static mut PREV: u8 = 7;
-    let mut rand_num = rand::random::<u8>() % 7;
-    if unsafe {PREV} == rand_num {
-        rand_num = rand::random::<u8>() % 7;
-    }
-    unsafe {PREV = rand_num;}
-    match rand_num {
-        0 => TetriminoI::new(),
-        1 => TetriminoJ::new(),
-        2 => TetriminoL::new(),
-        3 => TetriminoO::new(),
-        4 => TetriminoS::new(),
-        5 => TetriminoZ::new(),
-        6 => TetriminoT::new(),
-        _ => unreachable!(),
-    }
-}
+// fn create_new_tetrimino() -> Tetrimino {
+//     static mut PREV: u8 = 7;
+//     let mut rand_num = rand::random::<u8>() % 7;
+//     if unsafe {PREV} == rand_num {
+//         rand_num = rand::random::<u8>() % 7;
+//     }
+//     unsafe {PREV = rand_num;}
+//     match rand_num {
+//         0 => TetriminoI::new(),
+//         1 => TetriminoJ::new(),
+//         2 => TetriminoL::new(),
+//         3 => TetriminoO::new(),
+//         4 => TetriminoS::new(),
+//         5 => TetriminoZ::new(),
+//         6 => TetriminoT::new(),
+//         _ => unreachable!(),
+//     }
+// }
 
 fn write_to_file(content: &str, file_name: &str) -> io::Result<()> {
     let mut f = File::create(file_name)?;
