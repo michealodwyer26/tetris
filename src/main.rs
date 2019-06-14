3e758c214d5301eb0dd929ab31b306b2ad07dd0b
michealodwyer26 <michealodwyer26@gmail.com>
2019-06-14 14:10:13 +0100

    Implemented event handling.

--- src/main.rs
+++ src/main.rs
@@ -408,6 +408,43 @@ fn create_texture_rect<'a>(canvas: &mut Canvas<Window>, texture_creator: &'a Tex
     }
 }
 
+fn handle_events(tetris: &mut Tetris, quit: &mut bool, timer: &mut SystemTime, event_pump: &mut sdl2::EventPump) -> bool {
+    let mut make_permanent = false;
+    if let Some(ref mut piece) = tetris.current_piece {
+        let mut tmp_x = piece.x;
+        let mut tmp_y = piece.y;
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {*quit = true; break},
+                Event::KeyDown{keycode: Some(Keycode::Down), ..} => {*timer = SystemTime::now(); tmp_y += 1},
+                Event::KeyDown{keycode: Some(Keycode::Right), ..} => tmp_x += 1
+                Event::Keydown{keycode: Some(Keycode::Left), ..} => tmp_x -= 1
+                Event::Keydown{keycode: Some(Keycode::Up), ..} => piece.rotate(&tetris.game_map)
+                Event::Keydown{keycode: Some(Keycode::Space), ..} => {
+                    let x = piece.x; 
+                    let mut y = piece.y;
+                    while piece.change_position(&tetris.game_map, x, y +1) {
+                        y += 1;
+                    }
+                    make_permanent = true
+                }
+                _ => {}
+            }
+        }
+        if !make_permanent {
+            if !piece.change_position(&tetris.game_map, tmp_x, tmp_y) && tmp_y != piece.y {
+                make_permanent = true;
+            }
+        }
+    }
+    if make_permanent {
+        tetris.make_permanent();
+        *timer = SystemTime::now();
+    }
+    make_permanent
+}
+
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed.");
     let video_subsystem = sdl_context.video().expect("SDL video subsystem failed.");
